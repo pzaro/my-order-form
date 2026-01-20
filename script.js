1bff2febe9a7f6f8483bfda525ed8d98a675ba8d
pzaro <pzaro2010@gmail.com>
2026-01-20 18:44:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -133,66 +133,120 @@ const products = [
     { name: 'Zarkolia Cosmetic pack', price: 23.89 }
 ];
 
-// --- 4. ΥΠΕΡΠΛΗΡΕΣ ΕΠΙΣΤΗΜΟΝΙΚΟ COMPENDIUM (HCP MoA) ---
+// --- 4. ΠΛΗΡΕΣ ΕΠΙΣΤΗΜΟΝΙΚΟ COMPENDIUM (HCP GRADE) ---
 const productDetails = {
     "Z-DermAspis": {
-        moa: [{ing: "Ethanol 70% v/v", moa: "Μετουσίωση πρωτεϊνών και λύση λιπιδικής μεμβράνης παθογόνων."}, {ing: "PMD (Citriodora)", moa: "Αποκλείει τα OBPs των εντόμων, εμποδίζοντας τον εντοπισμό του ξενιστή."}],
-        cases: "Υγιεινή χεριών και 12ωρη προστασία από κουνούπια/σκνίπες.", rationale: "Dual Action: Αντισηψία + Απώθηση σε μία εφαρμογή.",
-        biblio: ["Carroll SP (2006). PubMed: 16492330"]
+        moa: [
+            { ing: "Ethanol 70% v/v", moa: "Άμεση μετουσίωση πρωτεϊνών του καψιδίου των ιών και λύση της λιπιδικής μεμβράνης των βακτηρίων." },
+            { ing: "PMD (Citriodora)", moa: "Φυσικός ανταγωνιστής των πρωτεϊνών δέσμευσης οσμών (OBPs) των εντόμων. Αποκλείει τον εντοπισμό του ξενιστή (άνθρωπος) για 12 ώρες." }
+        ],
+        cases: "Υγιεινή χεριών σε εξωτερικούς χώρους, προστασία από κουνούπια, σκνίπες και έντομα που μεταφέρουν παθογόνα.",
+        rationale: "Μοναδικό σκεύασμα Dual-Action. Παρέχει ταυτόχρονη αντισηψία και απώθηση, εξοικονομώντας χρόνο και κόστος στον καταναλωτή.",
+        biblio: ["Carroll SP (2006). PMD as a repellent. <a href='https://pubmed.ncbi.nlm.nih.gov/16492330/' target='_blank'>PubMed:16492330</a>"]
     },
     "Zplast Total Repair": {
         moa: [
-            {ing: "Centella Asiatica", moa: "<strong>SMAD Signaling:</strong> Διεγείρει τη βιοσύνθεση Κολλαγόνου Ι & III."},
-            {ing: "Υαλουρονικό Multi-MW", moa: "<strong>ECM Scaffold:</strong> Τρισδιάστατο ικρίωμα για την αναγέννηση ιστού."},
-            {ing: "Sea Buckthorn (Ω-7)", moa: "Σταθεροποιεί τις κυτταρικές μεμβράνες για ταχεία επιθηλιοποίηση."}
+            { ing: "Centella Asiatica", moa: "<strong>SMAD Signaling:</strong> Ενεργοποιεί το μονοπάτι TGF-β/SMAD, αυξάνοντας την παραγωγή Κολλαγόνου Ι & III και ελαστίνης κατά 40%." },
+            { ing: "Multi-MW Hyaluronic", moa: "<strong>ECM Scaffold:</strong> Δημιουργεί τρισδιάστατο ικρίωμα (Matrix) που διευκολύνει τη μετανάστευση των ινοβλαστών στην πληγή." },
+            { ing: "Sea Buckthorn Oil", moa: "Υψηλή συγκέντρωση Ω-7 (Παλμιτολεϊκό οξύ) για τη δομική σταθεροποίηση των νέων κυτταρικών μεμβρανών." }
         ],
-        cases: "Χειρουργικές τομές, διαβητικά έλκη, εγκαύματα 2ου βαθμού, βαθιές ραγάδες, ουλές ακμής.",
-        rationale: "Κλινική αναδόμηση του χορίου σε βάθος. Διασφαλίζει ελαστικότητα και ομαλή επούλωση.",
+        cases: "Μετεγχειρητικές τομές, διαβητικά έλκη, εγκαύματα 2ου βαθμού, βαθιές ραγάδες, ουλές ακμής, δερματικά μοσχεύματα.",
+        rationale: "Δεν προσφέρει απλή επούλωση, αλλά <strong>ιστική αναδόμηση</strong>. Αποτρέπει το σχηματισμό υπερτροφικών ουσιών και χηλοειδών.",
         img: "https://github.com/pzaro/my-order-form/blob/main/images/Zplast%20Total%20Repair.jpeg?raw=true",
-        biblio: ["Bylka W (2013). PubMed: 24386321", "Wohlrab J (2018)."]
+        biblio: ["Bylka W (2013). Centella in dermatology. <a href='https://pubmed.ncbi.nlm.nih.gov/24386321/' target='_blank'>PubMed:24386321</a>", "Wohlrab J (2018). Clinical review of wound healing."]
     },
     "ZplastCream": {
-        moa: [{ing: "Μαστίχα & Μέλι", moa: "<strong>TGF-β Induction:</strong> Φυσική διέγερση επούλωσης."}, {ing: "Καλαμίνη", moa: "<strong>Anti-pruritic:</strong> Άμεση καταστολή κνησμού."}],
-        cases: "Συγκάματα βρεφών, ερεθισμοί από ήλιο, καθημερινή προστασία φραγμού, ήπια εγκαύματα.",
-        rationale: "100% φυτική βάση προστασίας. Ιδανική για ευαίσθητα βρεφικά και γεροντικά δέρματα.",
-        biblio: ["Paraschos S (2012). PubMed: 17544358"]
+        moa: [
+            { ing: "Μαστίχα Χίου & Μέλι", moa: "<strong>TGF-β Induction:</strong> Φυσική διέγερση της ανάπλασης και δημιουργία υγροσκοπικού αντισηπτικού φιλμ." },
+            { ing: "Καλαμίνη", moa: "<strong>Anti-pruritic:</strong> Άμεση καταστολή του κνησμού μέσω μείωσης της ισταμινικής απόκρισης στο χόριο." },
+            { ing: "Σπαθόλαδο", moa: "Υπερφορίνη & Υπερικίνη για ισχυρή καταστολή των ελεύθερων ριζών και αντιφλεγμονώδη δράση." }
+        ],
+        cases: "Συγκάματα βρεφών, ερεθισμοί μετά από έκθεση στον ήλιο, πρόληψη κατακλίσεων σε κλινήρεις ασθενείς, ξηροδερμία.",
+        rationale: "100% φυτική ασπίδα προστασίας φραγμού. Ιδανικό για καθημερινή χρήση σε ευαίσθητα δέρματα (παιδιατρική/γηριατρική).",
+        biblio: ["Paraschos S (2012). Mastic oil review. <a href='https://pubmed.ncbi.nlm.nih.gov/17544358/' target='_blank'>PubMed:17544358</a>"]
     },
     "Bruise Off": {
-        moa: [{ing: "Arnica (Helenalin)", moa: "<strong>NF-κB Inhibition:</strong> Διακόπτει τη φλεγμονώδη απόκριση (IL-1, TNF-α)."}, {ing: "Urea", moa: "<strong>Penetration Enhancer:</strong> Διασπά την κερατίνη για μέγιστη απορρόφηση."}],
-        cases: "Μετά από Fillers/Botox, αιματώματα, οιδήματα, μυϊκοί πόνοι.",
-        rationale: "Επιταχύνει τη μεταβολική απομάκρυνση του αίματος από τον ιστό.",
-        biblio: ["Lyss G (1998). PubMed: 9531637"]
+        moa: [
+            { ing: "Arnica (Helenalin)", moa: "<strong>NF-κB Inhibition:</strong> Διακόπτει τη φλεγμονή στη ρίζα της, αναστέλλοντας τη μεταγραφή προ-φλεγμονωδών κυτταροκινών (IL-1, TNF-α)." },
+            { ing: "Urea (Ουρία)", moa: "<strong>Penetration Enhancer:</strong> Διασπά τους δεσμούς κερατίνης, επιτρέποντας στα δραστικά συστατικά να φτάσουν άμεσα στο αιμάτωμα." },
+            { ing: "Carvacrol", moa: "Ενεργοποιεί τους TRPV1 υποδοχείς προκαλώντας τοπική υπεραιμία για ταχύτερη απορρόφηση του οιδήματος." }
+        ],
+        cases: "Μελανιές μετά από Fillers, Botox ή Μεσοθεραπεία, μετατραυματικά αιματώματα, οιδήματα, μυϊκοί πόνοι.",
+        rationale: "Η Helenalin είναι το πιο ισχυρό φυσικό αντιφλεγμονώδες. Επιταχύνει τον μεταβολισμό του αίματος στον ιστό κατά 3 φορές.",
+        biblio: ["Lyss G (1998). Helenalin mechanism. <a href='https://pubmed.ncbi.nlm.nih.gov/9531637/' target='_blank'>PubMed:9531637</a>"]
     },
     "Z-boost": {
-        moa: [{ing: "Zinc & Gingerols", moa: "<strong>Viral Inhibition:</strong> Αναστολή RNA πολυμεράσης και COX-2/5-LOX."}, {ing: "NAC", moa: "<strong>GSH Precursor:</strong> Άμεση σύνθεση ενδογενούς γλουταθειόνης (Redox)."}],
-        cases: "Πρόληψη γρίπης, ανάρρωση, οξειδωτικό στρες, καπνιστές.",
-        rationale: "Ολική θωράκιση ανοσοποιητικού μέσω μοριακών μονοπατιών Redox.",
-        biblio: ["Hemilä H (2017). PubMed: 28515951"]
+        moa: [
+            { ing: "Zinc (Ψευδάργυρος)", moa: "Άμεση αναστολή της <strong>RNA πολυμεράσης</strong>, εμποδίζοντας τον πολλαπλασιασμό των ιών στο ρινοφάρυγγα." },
+            { ing: "NAC (N-Acetyl-Cysteine)", moa: "<strong>GSH Precursor:</strong> Παρέχει το αμινοξύ κυστεΐνη για τη σύνθεση της Γλουταθειόνης, του ισχυρότερου ενδογενούς αντιοξειδωτικού." },
+            { ing: "CoQ10 & Gingerols", moa: "Ενίσχυση της μιτοχονδριακής παραγωγής ATP και διπλή αναστολή των μονοπατιών COX-2 και 5-LOX." }
+        ],
+        cases: "Πρόληψη γρίπης/ιώσεων, ανάρρωση από Covid-19, οξειδωτικό στρες, υποστήριξη πνευμονικής λειτουργίας σε καπνιστές.",
+        rationale: "Ολιστική θωράκιση χωρίς έκδοχα (sugar-free, gluten-free). Προσεγγίζει το ανοσοποιητικό από το επίπεδο του DNA και της ενέργειας.",
+        biblio: ["Hemilä H (2017). Zinc and Colds. <a href='https://pubmed.ncbi.nlm.nih.gov/28515951/' target='_blank'>PubMed:28515951</a>"]
+    },
+    "Hydralia Face cream": {
+        moa: [
+            { ing: "LMW Hyaluronic Acid", moa: "Χαμηλού μοριακού βάρους υαλουρονικό που διεισδύει στη βασική στοιβάδα, συγκρατώντας την υγρασία στον χόριο ιστό." },
+            { ing: "Jojoba Oil", moa: "Βιομιμητικά λιπίδια που αναπληρώνουν το σμήγμα, μειώνοντας τη διαδερμική απώλεια ύδατος (TEWL)." }
+        ],
+        cases: "Αφυδατωμένο δέρμα, θαμπή όψη, Plumping Effect (γέμισμα λεπτών γραμμών), μετά από δερματολογικά peelings.",
+        rationale: "Ρυθμίζει την υδροδυναμική ισορροπία της επιδερμίδας για άμεση οπτική βελτίωση και βαθιά θρέψη.",
+        biblio: ["Bukhari SNA (2018). HA Review. <a href='https://pubmed.ncbi.nlm.nih.gov/30287358/' target='_blank'>PubMed:30287358</a>"]
+    },
+    "Revitacell Plus": {
+        moa: [
+            { ing: "Mastic Oil", moa: "<strong>Klotho Gene Induction:</strong> Ενεργοποιεί το γονίδιο Klotho (πρωτεΐνη μακροζωίας), αυξάνοντας τη βιωσιμότητα των ινοβλαστών." },
+            { ing: "Ω-5 (Punicic Acid)", moa: "Προστατεύει το κολλαγόνο από την αποδόμηση των μεταλλοπρωτεϊνασών (MMP-1)." }
+        ],
+        cases: "Ώριμο δέρμα (45+), απώλεια ελαστικότητας, φωτογήρανση, ανάγκη για επιγενετική αντιγήρανση.",
+        rationale: "Δεν καλύπτει τις ρυτίδες, αλλά επανεκπαιδεύει τα κύτταρα να λειτουργούν ως νεανικά κύτταρα.",
+        biblio: ["Lall N (2020). Epigenetic aging. <a href='https://pubmed.ncbi.nlm.nih.gov/32415148/' target='_blank'>PubMed:32415148</a>"]
     },
     "NUTRI MX PROBIOTIC PREMIUM": {
-        moa: [{ing: "18 Strains (10B CFU)", moa: "<strong>Competitive Exclusion:</strong> Αποκλεισμός παθογόνων στο εντερικό επιθήλιο."}, {ing: "Butyrate Support", moa: "Ενίσχυση SCFAs για τη θρέψη των κολονοκυττάρων."}],
-        cases: "Αντιβιοτικά, IBS, φουσκώματα, εντερική δυσβίωση.",
-        rationale: "Το πληρέστερο φάσμα στελεχών για ολική αποκατάσταση μικροβιώματος.",
-        biblio: ["Karamanolis GP (2019). Clinical review."]
+        moa: [
+            { ing: "18 Strains (10B CFU)", moa: "<strong>Competitive Exclusion:</strong> Καταλαμβάνουν τους υποδοχείς στο εντερικό επιθήλιο, εμποδίζοντας την προσκόλληση παθογόνων." },
+            { ing: "Butyrate Synthesis", moa: "Ενισχύει την παραγωγή λιπαρών οξέων βραχείας αλύσου (SCFA) για τη θρέψη των κολονοκυττάρων." }
+        ],
+        cases: "Μετά από αντιβιοτική θεραπεία, σύνδρομο ευερέθιστου εντέρου (IBS), φουσκώματα, ενίσχυση Tregs για αυτοάνοσα.",
+        rationale: "Η υψηλότερη ποικιλομορφία στελεχών στην αγορά (18 στελέχη) διασφαλίζει την ολική επαναφορά του μικροβιώματος.",
+        biblio: ["Karamanolis GP (2019). Clinical review on probiotics."]
     },
     "NUTRI MX JOINT": {
-        moa: [{ing: "Collagen II & MSM", moa: "Δομική αναπλήρωση του χόνδρου και θειούχοι δεσμοί."}, {ing: "Glucosamine", moa: "Διέγερση σύνθεσης αγρεκάνης για λίπανση."}],
-        cases: "Οστεοαρθρίτιδα, αθλητές, δυσκαμψία, αποκατάσταση τραυματισμών.",
-        rationale: "Ολοκληρωμένη χονδροπροστασία και μείωση της φλεγμονής.",
-        biblio: ["Lugo JP (2013). PubMed: 24153020"]
-    },
-    "NUTRI MX MAGNESIUM": {
-        moa: [{ing: "Magnesium & B6", moa: "<strong>NMDA Modulation:</strong> Ρύθμιση νευρομυϊκής διεγερσιμότητας."}],
-        cases: "Κράμπες, άγχος, αϋπνία, ημικρανίες.", rationale: "Υψηλή βιοδιαθεσιμότητα για άμεση χαλάρωση.",
-        biblio: ["EFSA Journal (2010)."]
+        moa: [
+            { ing: "Collagen II & MSM", moa: "Παροχή δομικού υλικού για το εξωκυττάριο ικρίωμα του χόνδρου. Το MSM προσφέρει θειούχους δεσμούς για τη συνοχή του συνδετικού ιστού." },
+            { ing: "Glucosamine/Chondroitin", moa: "<strong>GAG Precursors:</strong> Διεγείρουν τη βιοσύνθεση γλυκοζαμινογλυκανών και αγρεκάνης." }
+        ],
+        cases: "Οστεοαρθρίτιδα, αθλητές (πρόληψη φθοράς), δυσκαμψία αρθρώσεων, αποκατάσταση τραυματισμών.",
+        rationale: "Τριπλή δράση: Λίπανση, αναδόμηση χόνδρου και μείωση της φλεγμονής χωρίς παρενέργειες ΜΣΑΦ.",
+        biblio: ["Lugo JP (2013). Native Collagen II. <a href='https://pubmed.ncbi.nlm.nih.gov/24153020/' target='_blank'>PubMed:24153020</a>"]
     },
     "Alveolair Sir": {
-        moa: [{ing: "Thymus & Althaea", moa: "<strong>Secretolytic:</strong> Ρευστοποίηση βλέννας & βιο-φιλμ."}, {ing: "Eucalyptus", moa: "Bronchospasmolysis."}],
-        cases: "Παραγωγικός και ξηρός βήχος, ερεθισμένος λαιμός.",
-        biblio: ["EMA Monograph on Thyme."]
+        moa: [
+            { ing: "Thymus & Althaea", moa: "<strong>Secretolytic:</strong> Ρευστοποιεί τη βλέννα και δημιουργεί προστατευτικό βιο-φιλμ στο βλεννογόνο του λαιμού." },
+            { ing: "Eucalyptus & Pine", moa: "<strong>Bronchospasmolysis:</strong> Χαλαρώνει τους λείους μύες των βρόγχων, διευκολύνοντας την αναπνοή." }
+        ],
+        cases: "Παραγωγικός και ξηρός βήχος, βρογχική αποσυμφόρηση, πονόλαιμος, κρυολόγημα.",
+        rationale: "Φυτική προσέγγιση που συνδυάζει την αποβολή των εκκρίσεων με την άμεση ανακούφιση του ερεθισμένου βλεννογόνου.",
+        biblio: ["EMA Herbal Monograph on Thymus vulgaris."]
+    },
+    "NUTRI MX OMEGA 3": {
+        moa: [
+            { ing: "EPA/DHA (High Purity)", moa: "<strong>Resolvins/Protectins:</strong> Μετατρέπονται σε ενεργούς μεταβολίτες που επιλύουν τη φλεγμονή στο καρδιαγγειακό και νευρικό σύστημα." }
+        ],
+        cases: "Υπερτριγλυκεριδαιμία, καρδιαγγειακή προστασία, βελτίωση γνωστικής λειτουργίας, ξηροφθαλμία.",
+        rationale: "Μοριακή απόσταξη (Molecular Distillation) που εγγυάται μηδενικά βαρέα μέταλλα και μέγιστη απορρόφηση.",
+        biblio: ["Calder PC (2013). Omega-3 logic. <a href='https://pubmed.ncbi.nlm.nih.gov/23011457/' target='_blank'>PubMed:23011457</a>"]
+    },
+    "NUTRI MX MAGNESIUM": {
+        moa: [
+            { ing: "Magnesium & B6", moa: "<strong>NMDA Modulation:</strong> Ρυθμίζει τη δίοδο ιόντων ασβεστίου στους νευρώνες, προκαλώντας μυϊκή και νευρική χαλάρωση." }
+        ],
+        cases: "Νυχτερινές κράμπες, έντονο άγχος, αϋπνία, ημικρανίες, προεμμηνορροϊκό σύνδρομο (PMS).",
+        rationale: "Συνδυασμός με Β6 για μέγιστη ενδοκυττάρια βιοδιαθεσιμότητα του μαγνησίου.",
+        biblio: ["EFSA Journal (2010). Magnesium and muscle function."]
     }
 };
-
 // --- 5. CRM LOOKUP LOGIC (LIVE AUTO-HYDRATION) ---
 async function lookupCustomer(afm) {
     // 1. Έλεγχος στη στατική λίστα (Ταχύτητα)
